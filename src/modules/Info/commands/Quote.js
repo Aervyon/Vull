c2347c7984d3de3c493309bec365db4516b2f621
Null <36554880+VoidNulll@users.noreply.github.com>
2019-05-14 20:56:26 -0500

    Updated Quote

--- src/modules/Info/commands/Quote.js
+++ src/modules/Info/commands/Quote.js
@@ -12,27 +12,82 @@ class Quote extends Command {
             examples: ['quote 577706024459567125', 'quote 577706024459567125 Testing'],
         };
         this.argsMin = 1;
+        this.options.guildOnly = true;
+    }
+
+    async fetchMessage(msg, channel, mID) {
+        let mess;
+        try { // Fetch the message
+            mess = await this.axon.client.getMessage(channel.id, mID);
+        } catch (err) {
+            const er = err.message || err;
+            this.axon.Logger.error(er);
+            if (er.search(/(\[10008]: Unknown Message)$/) ) { // If there is no message
+                this.sendError(msg.channel, 'Message not found!');
+                return 'fail';
+            }
+            if (er.search(/(Missing Access)$/) ) { // If the bot does not have access to see that channe;
+                this.sendError(msg.channel, 'No permissions!');
+                return 'fail';
+            }
+            if (er.search(/invalid form body/i) && er.match(/(is not snowflake.)$/) ) { // If what the user sent is not a ID
+                this.sendError(msg.channel, 'Invalid ID. ID must be snowflake!');
+                return 'fail';
+            }
+            // Uh, idk what other errors to handle here.
+            this.sendError(msg.channel, 'Something went wrong and i have not a clue why or what.');
+            return 'fail';
+        }
+        return mess;
     }
 
     async execute( { msg, args } ) {
         let mess;
         let content;
         const embed = {};
-        let channel;
-        if (!args[1] ) {
-            channel = msg.channel;
-        }
+        let { channel } = msg;
         if (args[1] ) {
             channel = Resolver.channel(msg.channel.guild, args[1] );
             if (!channel) return this.sendError(msg.channel, 'Channel not found!');
         }
-        mess = await this.axon.client.getMessage(channel.id, args[0] );
-        if (!mess) return this.sendError(msg.channel, 'Message not found!');
-        const messg = await this.sendMessage(msg.channel, `${this.axon.configs.template.emote.loading} Restrucuring message!`);
-        if (mess.content) content = mess.content;
+        mess = await this.fetchMessage(msg, channel, args[0] ); // Fetch a message and handle errors
+        if (mess === 'fail') return Promise.resolve();
+        if (!mess) return this.sendError(msg.channel, 'Message not found!'); // This should never happen but eh
+        const messg = await this.sendMessage(msg.channel, `${this.axon.configs.template.emote.loading} Restrucuring message!`); // Alert that the message is being restrcutured
+        // Restructure
+        if (mess.content) {
+            let ncontent = mess.content;
+            if (mess.content.search(/<@!?\d+>/) ) {
+                ncontent = mess.content.replace(/<@!?|>/g, '');
+            }
+            content = ncontent;
+        }
+        const fmess = mess;
         if (!mess.embeds || mess.embeds.length === 0) {
             if (content) {
-                this.editMessage(messg, `${this.axon.configs.template.emote.success} Restructuring Success`);
+                await this.editMessage(messg, {
+                    content: `${this.axon.configs.template.emote.success} Restructuring Success!`,
+                    embed: {
+                        title: `Message: ${fmess.id}`,
+                        author: {
+                            icon_url: fmess.author.avatarURL,
+                            name: fmess.author.username,
+                        },
+                        fields: [
+                            {
+                                name: 'Channel ID',
+                                value: channel.id,
+                                inline: true,
+                            },
+                            {
+                                name: 'Message Link',
+                                value: `https://discordapp.com/channels/${msg.channel.guild.id}/${channel.id}/${fmess.id}`,
+                                inline: true,
+                            },
+                        ],
+                        color: this.axon.configs.template.embed.colors.help,
+                    },
+                } );
                 return this.sendMessage(msg.channel, content);
             }
             return this.editMessage(messg, `${this.axon.configs.template.emote.error} Empty message`);
@@ -49,7 +104,29 @@ class Quote extends Command {
 
         if (!embed && !content) return this.editMessage(messg, `${this.axon.configs.template.emote.error} Restructuring failed: Empty Message`);
 
-        await this.editMessage(messg, `${this.axon.configs.template.emote.success} Restructuring Success!`);
+        await this.editMessage(messg, {
+            content: `${this.axon.configs.template.emote.success} Restructuring Success!`,
+            embed: {
+                title: `Message: ${fmess.id}`,
+                author: {
+                    icon_url: fmess.author.avatarURL,
+                    name: fmess.author.username,
+                },
+                fields: [
+                    {
+                        name: 'Channel ID',
+                        value: channel.id,
+                        inline: true,
+                    },
+                    {
+                        name: 'Message Link',
+                        value: `https://discordapp.com/channels/${msg.channel.guild.id}/${channel.id}/${fmess.id}`,
+                        inline: true,
+                    },
+                ],
+                color: this.axon.configs.template.embed.colors.help,
+            },
+        } );
         const obj = {};
         if (content) obj.content = content;
         if (embed) obj.embed = embed;
