feac4a57b1ad02bee1e421b6e1f6a74b327d4bac
Null <36554880+VoidNulll@users.noreply.github.com>
2019-05-21 21:33:35 -0500

    Rewrote serverinfo command.
    Serverinfo command got rewrote. It also looks nicer than before hand. No package.json's were hurt in the remaking of this command

--- src/modules/Info/commands/index.js
+++ src/modules/Info/commands/index.js
@@ -1,3 +1,4 @@
 export { default as Game } from './Game';
 export { default as Who } from './Who';
 export { default as Quote } from './Quote';
+export { default as Serverinfo } from './Serverinfo';
