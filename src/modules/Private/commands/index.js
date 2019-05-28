4b956215ce52f4354dafbc40e58c28104a382d74
Null <36554880+VoidNulll@users.noreply.github.com>
2019-05-27 21:26:43 -0500

    Added ability to blacklist/un users/guilds
    Blacklisting priotizes users over guilds. Uses a standard resolver for both. If the user cannot be found and you provided a ID the bot will also search via rest for the user, this is not avaliable for guilds however.

--- src/modules/Private/commands/index.js
+++ src/modules/Private/commands/index.js
@@ -3,3 +3,5 @@ export { default as Guilds } from './Guilds';
 export { default as Execute } from './Exec';
 export { default as Genable } from './Genable';
 export { default as Gdisable } from './Gdisable';
+export { default as Blacklist } from './Blacklist';
+export { default as Unblacklist } from './Unblacklist';
