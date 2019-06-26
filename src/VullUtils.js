d4910c004446643bb1fe52f8771c142296ca7551
Null <36554880+VoidNulll@users.noreply.github.com>
2019-06-26 17:45:50 -0500

    Added jsDoc to the utils

--- src/VullUtils.js
+++ src/VullUtils.js
@@ -1,7 +1,23 @@
 /* eslint-disable no-magic-numbers */
-import { Utils } from 'axoncore';
+import axoncore from 'axoncore';
+const { Utils, AxonError } = axoncore;
 
+/**
+ * Custom Utils designed to help Vull.
+ *
+ * @class
+ * @extends axoncore.Utils
+ * */
 class VullUtils extends Utils {
+    /**
+     * @prop hrReg Regex to resolve hours
+     * @prop mReg Regex to resolve minutes
+     * @prop dReg Regex to resolve days
+     * @prop yReg Regex to resolve years
+     * @prop wReg Regex to resolve weeks
+     * @prop timeReg Regex used to slice useless portions of a time string off
+     * @prop {Object} nums Permissions numbers, used for calculating permissions.
+     * */
     constructor(...args) {
         super(...args);
         this.hrReg = /(h|hour|hr|hours|hrs)$/;
@@ -10,15 +26,97 @@ class VullUtils extends Utils {
         this.yReg = /(y|year|yr|yrs|years)$/;
         this.wReg = /(w|week|weeks|ws)$/;
         this.timeReg = /, 0 [a-z]{5,}/ig;
+        this.nums = {
+            createInstantInvite: 1,
+            kickMembers: 2,
+            banMembers: 4,
+            administrator: 8,
+            manageChannels: 16,
+            manageGuild: 32,
+            addReactions: 64,
+            viewAuditLogs: 128,
+            voicePrioritySpeaker: 256,
+            readMessages: 1024,
+            sendMessages: 2048,
+            sendTTSMessages: 4096,
+            manageMessages: 8192,
+            embedLinks: 16384,
+            attachFiles: 32768,
+            readMessageHistory: 65536,
+            mentionEveryone: 131072,
+            externalEmojis: 262144,
+            voiceConnect: 1048576,
+            voiceSpeak: 2097152,
+            voiceMuteMembers: 4194304,
+            voiceDeafenMembers: 8388608,
+            voiceMoveMembers: 16777216,
+            voiceUseVAD: 33554432,
+            changeNickname: 67108864,
+            manageNicknames: 134217728,
+            manageRoles: 268435456,
+            manageWebhooks: 536870912,
+            manageEmojis: 1073741824,
+            all: 2146958847,
+            allGuild: 2080374975,
+            allText: 805829713,
+            allVoice: 871366929,
+        };
     }
 
-    _isValidVullNum(ms) {
+    /**
+     * Calculate permissions using a object of perms
+     *
+     * @param {Object} data The permissions to calculate for
+     *
+     *  @returns {Object} Object containing the perms denied & allowed
+     */
+    calculate(data) {
+        let allow = 0;
+        let deny = 0;
+        for (const key of Object.keys(data) ) {
+            if (!this.nums[key] ) throw new AxonError(`Key ${key} not found!`, 'Enums');
+            if (data[key] === true) {
+                allow = Math.round(allow + this.nums[key] );
+            } else if (data[key] === false) {
+                deny = Math.round(deny + this.nums[key] );
+            }
+            if (key === 'all') {
+                if (data[key] === true) {
+                    allow = this.nums[key];
+                    break;
+                } else if (data[key] === false) {
+                    deny = this.nums[key];
+                    break;
+                }
+            }
+        }
+        return { allow, deny };
+    }
+
+    /**
+     * Checks if time is valid number
+     *
+     * @param {Number} ms The time in milliseconds.
+     *
+     * @private
+     *
+     * @memberof VullUtils
+     * */
+    static _isValidVullNum(ms) {
         if (Math.sign(ms) < 1) return 'MS cannot be negative or 0';
         if (isNaN(ms) ) return 'MS must be Number';
         if (!isFinite(ms) ) return 'MS cannot be Infinity';
         return true;
     }
 
+    /**
+     *Turns milliseconds to a readable string.
+     *
+     * @param {Number} ms Milliseconds to turn to a string.
+     *
+     * @memberof VullUtils
+     * @returns {String}
+     * */
     fromMS(ms) {
         const isValid = this._isValidVullNum(ms);
         if (isValid !== true) throw Error(isValid);
@@ -27,15 +125,42 @@ class VullUtils extends Utils {
         return date;
     }
 
+    /**
+     * Slices time string to not contain garbage.
+     *
+     * @param {String} timeString The time as string to simplify
+     * Time string must be a string like the output of VullUtils.fromMS
+     *
+     * @memberof VullUtils
+     * @returns {String}
+     * */
     sliceTime(timeString) {
         const time = timeString.replace(this.timeReg, '');
         return time;
     }
 
+    /**
+     * Nicely formats millisecond time.
+     *
+     * @param {Number} ms Milliseconds to turn to a nice string
+     *
+     * @memberof VullUtils
+     * @returns {String}
+     * */
     fullTimeFormat(ms) {
         return this.sliceTime(this.fromMS(ms) );
     }
 
+    /**
+     * Inits the muted role for a guild.
+     * Should only be used with mute command.
+     *
+     * @param {Object} guildConf The guilds database configuration.
+     * @param {Object} guild The guild to make the muted role for
+     *
+     * @memberof VullUtils
+     * @returns {String<Role.id>}
+     * */
     async initMutedRole(guildConf, guild) {
         const role = await guild.createRole( { name: 'V-Muted', color: 6842472, permissions: 0 }, 'Automatic mute setup');
         guild.channels.forEach(channel => {
@@ -46,6 +171,15 @@ class VullUtils extends Utils {
         return role.id;
     }
 
+    /**
+     * Turn a date string to milliseconds.
+     * Useful for functions requiring user input that need millisecond time.
+     *
+     * @param {String} dateString The string to get the time from.
+     *
+     * @memberof VullUtils
+     * @returns {Number}
+     */
     toMS(dateString) {
         let time;
         if (dateString.match(this.hrReg) ) {
